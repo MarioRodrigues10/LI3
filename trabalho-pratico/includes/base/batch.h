9fc73a13eafac4a71f65f2eb7b42fce1debe166d
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-10-23 22:33:35 +0100

    Run formatter

--- trabalho-pratico/includes/base/batch.h
+++ trabalho-pratico/includes/base/batch.h
@@ -5,7 +5,7 @@
  * @brief Function to execute batch mode
  * @param argv the command line arguments
  * return 0 if success, -1 if fail
-*/
+ */
 int batch(char **argv);
 
 #endif
--- trabalho-pratico/includes/utils/utils.h
+++ trabalho-pratico/includes/utils/utils.h
@@ -1,12 +1,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-
 /**
  * @brief Function to setup catalog
  * @param folder the folder where the csv is located
  * return 0 if success, -1 if fail
-*/
+ */
 int setup_catalogs(char* folder);
 
 #endif
