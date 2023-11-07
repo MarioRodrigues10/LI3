ce426a1662218b550fc5270f98723c13941a4c41
Afonso Santos <afonso.ds33@gmail.com>
2023-11-07 12:47:43 +0000

    Add the insert fuction in catalogs

--- trabalho-pratico/Resultados/README.md
+++
@@ -1,3 +0,0 @@
-Pasta onde devem ser criados os ficheiros individuais com o resultado
-da execução de cada comando. Esses ficheiros não deverão ser guardaos
-no repositório GitHub (conforme definido no .gitignore).
--- trabalho-pratico/src/catalogs/flight_catalog.c
+++ trabalho-pratico/src/catalogs/flight_catalog.c
@@ -15,6 +15,11 @@ FlIGHTS_CATALOG create_flights_catalog() {
   return new_catalog;
 }
 
+void add_flight_to_catalog(FlIGHTS_CATALOG flights_catalog, FLIGHT flight,
+                           gpointer key) {
+  g_hash_table_insert(flights_catalog->flights, key, flight);
+}
+
 void free_flights_catalog(FlIGHTS_CATALOG flights_catalog) {
   g_hash_table_destroy(flights_catalog->flights);
   g_free(flights_catalog);
