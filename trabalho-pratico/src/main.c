a0c1b464eda0afc55cdf0c24de92fe1e7f4125c4
Mário Rodrigues <93675410+MarioRodrigues10@users.noreply.github.com>
2024-01-08 11:28:27 +0000

    chore: Makefile (#59)
    * chore: Makefile
    
    * fix: remove clock and memory
    
    * chore: add more flags

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -7,13 +7,11 @@
 
 int main(int argc, char **argv) {
   // ./programa-principal dataset/data_clean dataset/input.txt
-
   if (argc != 3) {
     printf("Invalid number of arguments\n");
     return 1;
   } else {
     input_handler(argv);
   }
-
   return 0;
 }
